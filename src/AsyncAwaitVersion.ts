cab7e730239f7b4f6b50efd1c1bf96f7c4be2a55
Melokuhle <melokuhlemaphisa99@gmail.com>
2025-10-22 09:34:31 +0200

    feat: I did a refactor of the Promise code to use async/await and handled errors with try...catch in AsyncAwaitVersion.ts. In package.json, I capitalized the files in script.

--- src/AsyncAwaitVersion.ts
+++ src/AsyncAwaitVersion.ts
@@ -1,2 +1,32 @@
-import http from 'http';
+import axios from "axios";
 
+async function fetchWeather() {
+  const res = await axios.get(
+    "https://api.open-meteo.com/v1/forecast?latitude=35&longitude=139&hourly=temperature_2m"
+  );
+  return res.data;
+}
+
+async function fetchNews() {
+  const res = await axios.get("https://dummyjson.com/posts");
+  return res.data.posts;
+}
+
+async function displayData() {
+  try {
+    const weather = await fetchWeather();
+    const news = await fetchNews();
+
+    console.log("Async/Await Weather:", weather);
+    console.log("Async/Await News:", news.slice(0, 5));
+
+    // Run simultaneously
+    const [weather2, news2] = await Promise.all([fetchWeather(), fetchNews()]);
+    console.log("Promise.all Async/Await Weather:", weather2);
+    console.log("Promise.all Async/Await News:", news2.slice(0, 5));
+  } catch (err) {
+    console.error("Async/Await Error:", err);
+  }
+}
+
+displayData();
